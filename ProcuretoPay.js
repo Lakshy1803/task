b60d32f230ecbef1a9849b054056c33257c0a663
Lakshy1803 <76198405+Lakshy1803@users.noreply.github.com>
2024-03-08 15:50:37 +0530

    Add files via upload

---
+++ ProcuretoPay.js
@@ -0,0 +1,22 @@
+var div = document.getElementById("l3");
+var display = 1;
+function hideShow() {
+  if (display == 1) {
+    div.style.display = "block";
+    display = 0;
+  } else {
+    div.style.display = "none";
+    display = 1;
+  }
+}
+var div4 = document.getElementById("l4");
+var display4 = 1;
+function hideShow4() {
+  if (display4 == 1) {
+    div4.style.display = "block";
+    display4 = 0;
+  } else {
+    div4.style.display = "none";
+    display4 = 1;
+  }
+}
